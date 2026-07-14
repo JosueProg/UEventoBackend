30c0d9fe93acc13f6033e80dda699c862d126006
Josue <josueidrovo123@gmail.com>
2026-07-14 12:37:29 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/ComentariosController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UEventoBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ComentariosController : ControllerBase
+    {
+    }
+}
