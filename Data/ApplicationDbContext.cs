5b010cfe93d0dbdbebe088a517d914915aee7493
Josue <josueidrovo123@gmail.com>
2026-07-16 14:47:48 -0500

    Controller Comentarios terminado, a espera del resto de controllers para unificar todo

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UEventoBackend.Models;
+
+namespace UEventoBackend.Data
+{
+
+    public class ApplicationDbContext : DbContext
+    {
+              public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Comentario> Comentarios { get; set; }
+    }
+}
---
+++ Dtos/ComentarioDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace UEventoBackend.Dtos
+{
+    public class ComentarioDTO
+    {
+         
+        
+            public string Estudiante { get; set; } = string.Empty;
+            public string Contenido { get; set; } = string.Empty;
+            public int Calificacion { get; set; }
+            public bool Anonimo { get; set; }
+            public int EventoId { get; set; }
+        }
+    }
+
+
+
