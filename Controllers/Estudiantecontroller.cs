c59f9eb8b8740e2f28aecc80e514c72d63eaacc3
dasani14 <108086061+dasani14@users.noreply.github.com>
2026-07-17 13:41:08 -0500

    Add files via upload

---
+++ Controllers/Estudiantecontroller.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UEventoBackend.Models;
+
+[Authorize]
+[Route("api/[controller]")]
+[ApiController]
+public class EstudianteController : ControllerBase
+{
+    
+
+
+}
