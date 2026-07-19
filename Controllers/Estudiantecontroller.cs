52db6a6cb589c560a69b72aedb238bf1341af989
dasani14 <vetmario17@gmail.com>
2026-07-18 21:35:59 -0500

    Actualizacion controller estudiantes

--- Controllers/Estudiantecontroller.cs
+++ Controllers/Estudiantecontroller.cs
@@ -1,14 +1,54 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using UEventoBackend.Data;
 using UEventoBackend.Models;
 
-[Authorize]
-[Route("api/[controller]")]
-[ApiController]
-public class EstudianteController : ControllerBase
+namespace UEventoBackend.Controllers
 {
-    
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class Estudiantecontroller : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
 
+        public Estudiantecontroller(ApplicationDbContext context)
+        {
+            _context = context;
+        }
 
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Estudiante>>> GetEstudiantes()
+        {
+            return await _context.Estudiantes.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Estudiante>> PostEstudiante(Estudiante estudiante)
+        {
+            _context.Estudiantes.Add(estudiante);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetEstudiantes), new { id = estudiante.Id }, estudiante);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEstudiante(int id, Estudiante estudiante)
+        {
+            if (id != estudiante.Id) return BadRequest();
+            _context.Entry(estudiante).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEstudiante(int id)
+        {
+            var estudiante = await _context.Estudiantes.FindAsync(id);
+            if (estudiante == null) return NotFound();
+            _context.Estudiantes.Remove(estudiante);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
 }
